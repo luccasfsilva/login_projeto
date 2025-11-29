6d11af90dbe71508ddb37a494a4e1f145d57a183
Lucas Fernandes da Silva <101838528+luccasfsilva@users.noreply.github.com>
2025-11-29 00:17:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js";
 
-const supabaseUrl = "https://prcsnmjnhvisrzhlzshs.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InByY3NubWpuaHZpc3J6aGx6c2hzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQyNTY5MjcsImV4cCI6MjA3OTgzMjkyN30.cQpd7xq1KVtnyF7ycMwrJzOcdhpmqob3jqt07wud35g";
+const supabase = createClient(
+  "https://prcsnmjnhvisrzhlzshs.supabase.co",
+  "SUA_ANON_KEY"
+);
 
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-// LOGIN
-const form = document.querySelector("form");
+const form = document.getElementById("login-form");
 
 form.addEventListener("submit", async (e) => {
     e.preventDefault();
 
-    const email = document.querySelector('input[type="email"]').value;
-    const password = document.querySelector('input[type="password"]').value;
+    const email = form.querySelector('input[type="email"]').value;
+    const password = form.querySelector('input[type="password"]').value;
 
     const { error } = await supabase.auth.signInWithPassword({
         email,
-        password,
+        password
     });
 
     if (error) {
         alert("Erro: " + error.message);
     } else {
-        alert("Login realizado com sucesso ✅");
-        // redirecionar
+        alert("Login realizado ✅");
         window.location.href = "dashboard.html";
     }
 });
