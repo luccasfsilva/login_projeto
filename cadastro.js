93d4796224f4f4822c90ad2fc31917968cc9982e
Lucas Fernandes da Silva <101838528+luccasfsilva@users.noreply.github.com>
2025-11-29 01:00:28 -0300

    cadastro.js

---
+++ cadastro.js
@@ -0,0 +1,33 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js";
+
+const supabase = createClient(
+  "https://prcsnmjnhvisrzhlzshs.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InByY3NubWpuaHZpc3J6aGx6c2hzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQyNTY5MjcsImV4cCI6MjA3OTgzMjkyN30.cQpd7xq1KVtnyF7ycMwrJzOcdhpmqob3jqt07wud35g"
+);
+
+const form = document.getElementById("signup-form");
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const email = form.querySelector('input[type="email"]').value;
+    const password = form.querySelector('input[type="password"]').value;
+
+    const { error } = await supabase.auth.signUp({
+        email,
+        password
+    });
+
+    if (error) {
+        msg.textContent = "Erro: " + error.message;
+        msg.style.color = "red";
+    } else {
+        msg.textContent = "Conta criada ✅ Verifique seu email!";
+        msg.style.color = "green";
+
+        setTimeout(() => {
+            window.location.href = "index.html";
+        }, 3000);
+    }
+});
