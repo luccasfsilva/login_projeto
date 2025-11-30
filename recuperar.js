5e2886f3807a661290e49e1551907af43b01a556
Lucas Fernandes da Silva <101838528+luccasfsilva@users.noreply.github.com>
2025-11-29 23:41:08 -0300

    recuperar.js

--- recuperar.js
+++ recuperar.js
@@ -5,21 +5,23 @@ const supabase = createClient(
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InByY3NubWpuaHZpc3J6aGx6c2hzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQyNTY5MjcsImV4cCI6MjA3OTgzMjkyN30.cQpd7xq1KVtnyF7ycMwrJzOcdhpmqob3jqt07wud35g"
 );
 
-const form = document.getElementById("reset-form");
+const form = document.getElementById("recuperar-form");
 const msg = document.getElementById("msg");
 
 form.addEventListener("submit", async (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const email = form.querySelector("input").value;
+  const email = form.querySelector("input").value;
 
-    const { error } = await supabase.auth.resetPasswordForEmail(email, {
-        redirectTo: "https://luccasfsilva.github.io/login_projeto/nova-senha.html"
-    });
+  const { error } = await supabase.auth.resetPasswordForEmail(email, {
+    redirectTo: "https://luccasfsilva.github.io/login_projeto/nova-senha.html"
+  });
 
-    if (error) {
-        msg.textContent = "Erro: " + error.message;
-    } else {
-        msg.textContent = "Email enviado ✅ Verifique sua caixa de entrada.";
-    }
+  if (error) {
+    msg.textContent = "Erro: " + error.message;
+    msg.style.color = "red";
+  } else {
+    msg.textContent = "Email enviado ✅ Verifique sua caixa de entrada.";
+    msg.style.color = "green";
+  }
 });
